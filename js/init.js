ec95756a3c4d2feed66bad7e8646cd09a5175d57
cubap <cubap@slu.edu>
2017-08-25 17:00:23 -0500

    Check uri for any announcements
    Hitting firebase directly at the moment
    because of funny CORS where /id seems
    to work fine, but not /messages.

--- js/init.js
+++ js/init.js
@@ -2,24 +2,102 @@ angular.module('inbox',['ngRoute'])
     .controller('homeController', function($scope){
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+
+    $('.tooltipped').tooltip({delay: 50});
+
+            function scrollTo (event) {
+                function easeInOutQuad (t) {
+                    return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+                }
+                let a = event.target;
+                while (a && a.tagName !== "A") {
+                    a = a.parentElement;
+                }
+                let hash = a.hash;
+                event.preventDefault();
+                let target = document.getElementById(hash.substring(1));
+                // target.scrollIntoView({ behavior: 'smooth'});
+
+                const start = window.pageYOffset;
+                const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
+
+                const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+                const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
+                const destinationOffset = target.offsetTop;
+                const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
+
+                if ('requestAnimationFrame' in window === false) {
+                    window.scroll(0, destinationOffsetToScroll);
+                    return;
+                }
+
+                function scroll () {
+                    const now = 'now' in window.performance ? performance.now() : new Date().getTime();
+                    const time = Math.min(1, ((now - startTime) / 500));
+                    const timeFunction = easeInOutQuad(time);
+                    window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
+
+                    if (window.pageYOffset === destinationOffsetToScroll) {
+                        return;
+                    }
+                    requestAnimationFrame(scroll);
+                }
+                scroll();
+            }
+            let smooths = document.querySelectorAll('a.smooth');
+            for (let item of smooths) {
+                item.addEventListener('click', scrollTo);
+            }
+
+
 })
-.config(function($routeProvider, $locationProvider){
+    .controller('announcementsController',function($scope,$http){
+        $scope.checkUri = function(){
+            let uri = $scope.uri;
+            if (!uri) return false;
+            uri = 'https://rerum-inbox.firebaseio.com/messages.json?orderBy="target"&equalTo="'+uri+'"';
+            let promise = $http({
+                url:uri,
+                method:'GET'
+            })
+                .then(function(res){
+                    $scope.announcements = res.data;
+            }, function(){});
+        };
+        $scope.announcement = {
+    "@context":"https://iiif.io/api/presentation/2/context.json",
+    "@type":"Announce",
+    "target":"http://www.e-codices.ch/metadata/iiif/fcc-0020/manifest.json",
+    "motivation":"supplementing",
+    "actor":{"@id":"https://scta.info","label":"SCTA"},
+    "object":{"@id":"https://scta.info/iiif/lombardsententia/zbsSII72/ranges/toc/wrapper",
+        "@type":"sc:Range",
+        "attribution":"Data provided by the Scholastic Commentaries and Texts Archive",
+        "description":"A Table of Contents for lombardsententia/zbsSII72",
+        "license":"https://creativecommons.org/licenses/by-sa/4.0/",
+        "logo":"https://scta.info/logo.png"},
+    "published":"2017-08-23 20:15:52 UTC"
+};
+    $('.tooltipped').tooltip({delay: 50});
+})
+.config(function($routeProvider, $locationProvider, $httpProvider){
+    $httpProvider.defaults.useXDomain = true;
+
     $routeProvider
     .when('/',{
         templateUrl: 'partials/home.html',
         controller: 'homeController'
     })
+        .when('/boop',{
+        template: '<div>boop</div>',
+        controller: 'homeController'
+    })
         .when('/announcements',{
             templateUrl: 'partials/announcements.html',
-        controller: 'homeController',
-    resolve: {
-      // I will cause a 1 second delay
-      delay: function($q, $timeout) {
-        var delay = $q.defer();
-        $timeout(delay.resolve, 1000);
-        return delay.promise;
-      }
-    }
-    });
+        controller: 'announcementsController'
+    })
+        .otherwise({
+            redirectTo: "/"
+        });
 })
 ;
